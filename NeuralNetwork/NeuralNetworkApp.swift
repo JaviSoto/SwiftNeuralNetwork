a80345b8730e97d8f73895738c345cfc11b63a61
Javier Soto <ios@javisoto.es>
2022-12-14 16:18:34 -0800

    Initial Commit

---
+++ NeuralNetwork/NeuralNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuralNetworkApp.swift
+//  NeuralNetwork
+//
+//  Created by Javier Soto on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct NeuralNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
